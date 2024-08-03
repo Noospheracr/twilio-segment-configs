91b89ae89c7faee4f2fd5ebf3fead5abb6b783b1
Segment Git Sync <friends@segment.com>
2024-08-03 03:34:04 +0000

    Full sync

---
+++ users/ahJXC8BL3qW2Ccmew34yvg.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-ahJXC8BL3qW2Ccmew34yvg
+  id = "ahJXC8BL3qW2Ccmew34yvg"
+}
+
+resource "segment_user" "id-ahJXC8BL3qW2Ccmew34yvg" {
+  email = "max@noospheracr.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "rEsXiL8ETmd1mbsDgNczyX"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFMmZbyzdEWmE6k8JVgKAiE"
+    },
+  ]
+}
