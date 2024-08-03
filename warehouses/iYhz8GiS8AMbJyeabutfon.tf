a7bab6bb3fbac95ec2faf382f4b7b4734d5d2804
Segment Git Sync <friends@segment.com>
2024-08-03 05:05:22 +0000

    Full sync

---
+++ warehouses/iYhz8GiS8AMbJyeabutfon.tf
@@ -0,0 +1,13 @@
+import {
+  to = segment_warehouse.id-iYhz8GiS8AMbJyeabutfon
+  id = "iYhz8GiS8AMbJyeabutfon"
+}
+
+resource "segment_warehouse" "id-iYhz8GiS8AMbJyeabutfon" {
+  enabled = true
+  metadata = {
+    id = "kwX50Df0hr"
+  }
+  name     = "BigQuery Storage Destination"
+  settings = "{\"gc-project\":\"wain-402204\",\"location\":\"US\"}"
+}
