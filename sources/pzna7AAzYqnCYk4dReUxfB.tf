b696f8a09e0156ce7bfd49ea2590882ccd60b940
Segment Git Sync <friends@segment.com>
2024-08-03 05:05:14 +0000

    Full sync

---
+++ sources/pzna7AAzYqnCYk4dReUxfB.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-pzna7AAzYqnCYk4dReUxfB
+  id = "pzna7AAzYqnCYk4dReUxfB"
+}
+
+resource "segment_source" "id-pzna7AAzYqnCYk4dReUxfB" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "dBg7wLNWBb"
+  }
+  name     = "Personas Wain"
+  settings = "{}"
+  slug     = "personas_wain"
+}
