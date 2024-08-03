d441b53bd7a750bf83f045faf98990d3778f0392
Segment Git Sync <friends@segment.com>
2024-08-03 03:34:00 +0000

    Full sync

---
+++ sources/eELkQDVozf2pWCGPLtcdhj.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-eELkQDVozf2pWCGPLtcdhj
+  id = "eELkQDVozf2pWCGPLtcdhj"
+}
+
+resource "segment_source" "id-eELkQDVozf2pWCGPLtcdhj" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "78PbvP2yv8"
+  }
+  name     = "BigQuery Reverse ETL Source [wain-402204]"
+  settings = "{\"location\":\"US\"}"
+  slug     = "bigquery_reverse_etl_source_wain_402204"
+}
---
+++ sources/nzhFNktWQV9yVTcJyQn3FS.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-nzhFNktWQV9yVTcJyQn3FS
+  id = "nzhFNktWQV9yVTcJyQn3FS"
+}
+
+resource "segment_source" "id-nzhFNktWQV9yVTcJyQn3FS" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "EfKbe2yt0J"
+  }
+  name     = "Klaviyo"
+  settings = "{}"
+  slug     = "klaviyo"
+}
