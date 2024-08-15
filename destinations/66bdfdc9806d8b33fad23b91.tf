ca27737b5bc88718453b5392223f28a7c219a57a
Segment Git Sync <friends@segment.com>
2024-08-15 13:08:27 +0000

    Destination '66bdfdc9806d8b33fad23b91' created by max@noospheracr.com

---
+++ destinations/66bdfdc9806d8b33fad23b91.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66bdfdc9806d8b33fad23b91
+  id = "66bdfdc9806d8b33fad23b91"
+}
+
+resource "segment_destination" "id-66bdfdc9806d8b33fad23b91" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "650bdf1a62fb34ef0a8058e1"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Klaviyo (Actions)"
+  settings  = "{\"api_key\":\"\"}"
+  source_id = "DgC1LAHwYAcvPdsWdh7jV"
+}
