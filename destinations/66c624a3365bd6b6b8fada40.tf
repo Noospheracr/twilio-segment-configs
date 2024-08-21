279158f0b5b6ce42ff754a9b259c005e8ff182ec
Segment Git Sync <friends@segment.com>
2024-08-21 17:32:21 +0000

    Destination '66c624a3365bd6b6b8fada40' created by internal-service@segment.com

---
+++ destinations/66c624a3365bd6b6b8fada40.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_destination.id-66c624a3365bd6b6b8fada40
+  id = "66c624a3365bd6b6b8fada40"
+}
+
+resource "segment_destination" "id-66c624a3365bd6b6b8fada40" {
+  enabled = true
+  metadata = {
+    contacts = [
+      {
+      },
+    ]
+    id                = "618ac81d0dff5bb5207845a5"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name      = null
+  settings  = "{\"apiKey\":\"••••••••••aP50\"}"
+  source_id = "DgC1LAHwYAcvPdsWdh7jV"
+}
