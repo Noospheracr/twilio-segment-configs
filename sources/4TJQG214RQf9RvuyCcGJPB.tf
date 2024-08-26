40b8ad06a0f47d5074f5906d111d07ac7a7687ea
Segment Git Sync <friends@segment.com>
2024-08-26 18:15:47 +0000

    Source '4TJQG214RQf9RvuyCcGJPB' created by max@noospheracr.com

---
+++ sources/4TJQG214RQf9RvuyCcGJPB.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-4TJQG214RQf9RvuyCcGJPB
+  id = "4TJQG214RQf9RvuyCcGJPB"
+}
+
+resource "segment_source" "id-4TJQG214RQf9RvuyCcGJPB" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "sTypQz3Fd2"
+  }
+  name     = "Customer.io"
+  settings = "{}"
+  slug     = "customerio"
+}
