448ac0caffa9f0443d4c6d466215c9b4a6056ecd
Segment Git Sync <friends@segment.com>
2024-08-26 18:11:27 +0000

    Destination '66ccc54d7fd656aa925029c6' created by max@noospheracr.com

---
+++ destinations/66ccc54d7fd656aa925029c6.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66ccc54d7fd656aa925029c6
+  id = "66ccc54d7fd656aa925029c6"
+}
+
+resource "segment_destination" "id-66ccc54d7fd656aa925029c6" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "5f7dd78fe27ce7ff2b8bfa37"
+    partner_owned     = true
+    region_endpoints  = ["EU", "US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Customer.IO Destination"
+  settings  = "{\"accountRegion\":\"US 🇺🇸\",\"apiKey\":\"\",\"siteId\":\"\"}"
+  source_id = "DgC1LAHwYAcvPdsWdh7jV"
+}
