be2ed538e2c080798f1d28a0cc4f31dbcb822d08
Segment Git Sync <friends@segment.com>
2024-08-26 18:12:41 +0000

    Destination '66ccc54d7fd656aa925029c6' modified by max@noospheracr.com

--- destinations/66ccc54d7fd656aa925029c6.tf
+++ destinations/66ccc54d7fd656aa925029c6.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-66ccc54d7fd656aa925029c6" {
-  enabled = false
+  enabled = true
   metadata = {
     contacts          = null
     id                = "5f7dd78fe27ce7ff2b8bfa37"
@@ -13,6 +13,6 @@ resource "segment_destination" "id-66ccc54d7fd656aa925029c6" {
     supported_regions = ["us-west-2", "eu-west-1"]
   }
   name      = "Customer.IO Destination"
-  settings  = "{\"accountRegion\":\"US 🇺🇸\",\"apiKey\":\"\",\"siteId\":\"\"}"
+  settings  = "{\"accountRegion\":\"US 🇺🇸\",\"apiKey\":\"••••••••••6bff\",\"siteId\":\"••••••••••b686\"}"
   source_id = "DgC1LAHwYAcvPdsWdh7jV"
 }
