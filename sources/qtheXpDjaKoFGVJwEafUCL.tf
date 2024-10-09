19ea3b766c9c3809e1043ac21890554a117920b8
Segment Git Sync <friends@segment.com>
2024-10-09 05:46:56 +0000

    Source 'qtheXpDjaKoFGVJwEafUCL' created by max@noospheracr.com

---
+++ sources/qtheXpDjaKoFGVJwEafUCL.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-qtheXpDjaKoFGVJwEafUCL
+  id = "qtheXpDjaKoFGVJwEafUCL"
+}
+
+resource "segment_source" "id-qtheXpDjaKoFGVJwEafUCL" {
+  enabled = false
+  labels  = null
+  metadata = {
+    id = "oQ5dZPW0Ii"
+  }
+  name     = "Salesforce Marketing Cloud"
+  settings = "{}"
+  slug     = "salesforce_marketing_cloud"
+}
