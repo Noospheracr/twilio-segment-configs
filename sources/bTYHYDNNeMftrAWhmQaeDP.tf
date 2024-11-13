182af8860dad971dbb77d20b9aac02994cd4ecc5
Segment Git Sync <friends@segment.com>
2024-11-13 00:37:34 +0000

    Source 'bTYHYDNNeMftrAWhmQaeDP' created by max@noospheracr.com

---
+++ sources/bTYHYDNNeMftrAWhmQaeDP.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-bTYHYDNNeMftrAWhmQaeDP
+  id = "bTYHYDNNeMftrAWhmQaeDP"
+}
+
+resource "segment_source" "id-bTYHYDNNeMftrAWhmQaeDP" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "78PbvP2yv8"
+  }
+  name     = "Entity Group: bigquery in spa_ko1iuf5zTVK4TZwq6gGVLz"
+  settings = "{\"location\":\"US\"}"
+  slug     = "entity-group-bigquery-spa_ko1iuf5ztvk4tzwq6ggvlz"
+}
