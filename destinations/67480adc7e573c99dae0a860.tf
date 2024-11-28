1809f9d6688724df6037621ae80cd03de092d37c
Segment Git Sync <friends@segment.com>
2024-11-28 06:20:09 +0000

    Destination '67480adc7e573c99dae0a860' modified by max@noospheracr.com

--- destinations/67480adc7e573c99dae0a860.tf
+++ destinations/67480adc7e573c99dae0a860.tf
@@ -13,6 +13,6 @@ resource "segment_destination" "id-67480adc7e573c99dae0a860" {
     supported_regions = ["us-west-2", "eu-west-1"]
   }
   name      = "Facebook Custom Audiences (Actions)"
-  settings  = "{\"retlAdAccountId\":\"\"}"
+  settings  = "{\"retlAdAccountId\":\"383190990833946\"}"
   source_id = "eELkQDVozf2pWCGPLtcdhj"
 }
