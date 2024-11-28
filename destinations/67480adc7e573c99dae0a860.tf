92a08354f1bc86595f46a3ecc94e8d38203120fa
Segment Git Sync <friends@segment.com>
2024-11-28 06:17:01 +0000

    Destination '67480adc7e573c99dae0a860' created by max@noospheracr.com

---
+++ destinations/67480adc7e573c99dae0a860.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-67480adc7e573c99dae0a860
+  id = "67480adc7e573c99dae0a860"
+}
+
+resource "segment_destination" "id-67480adc7e573c99dae0a860" {
+  enabled = true
+  metadata = {
+    contacts          = null
+    id                = "645d5fc12eb891cf0a93fe4b"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Facebook Custom Audiences (Actions)"
+  settings  = "{\"retlAdAccountId\":\"\"}"
+  source_id = "eELkQDVozf2pWCGPLtcdhj"
+}
