d081ac0d1eb9e2ce2633ac90b3ba941bb32ae130
Segment Git Sync <friends@segment.com>
2024-11-28 16:51:00 +0000

    Destination Subscription '6678d6bdb9da1ca7b88733f1:jnXu7DA3oVZpAcNzRxUcJ5' enabled by max@noospheracr.com

--- destination_subscriptions/6678d6bdb9da1ca7b88733f1_jnXu7DA3oVZpAcNzRxUcJ5.tf
+++ destination_subscriptions/6678d6bdb9da1ca7b88733f1_jnXu7DA3oVZpAcNzRxUcJ5.tf
@@ -6,7 +6,7 @@ import {
 resource "segment_destination_subscription" "id-6678d6bdb9da1ca7b88733f1_jnXu7DA3oVZpAcNzRxUcJ5" {
   action_id      = "drUNmF6UifiVmB9NStLWS1"
   destination_id = "6678d6bdb9da1ca7b88733f1"
-  enabled        = false
+  enabled        = true
   model_id       = null
   name           = "Group Identify User"
   settings       = "{\"group_id\":{\"@path\":\"$.groupId\"},\"traits\":{\"@path\":\"$.traits\"}}"
